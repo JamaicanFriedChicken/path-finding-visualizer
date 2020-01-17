741f20345dcf7b934fad35e26252a1b68d9ba8fd
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2020-01-17 17:41:58 +0800

    initial draft of path finding visualizer

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Node from "./Node/Node";
+
+import "./PathfindingVisualizer.css";
+
+export default class PathfindingVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nodes: []
+        };
+    }
+
+    componentDidMount() {
+        const nodes = [];
+        for (let row = 0; row < 35; row++) {
+            const currentRow = [];
+            for (let col = 0; col < 50; col++) {
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({ nodes });
+    }
+
+    render() {
+        const { nodes } = this.state;
+        console.log(nodes);
+
+        return (
+            <div className="grid">
+                {nodes.map((row, rowIdx) => {
+                    return (
+                        <div key={rowIdx}>
+                            {row.map((node, nodeIdx) => (
+                                <Node></Node>
+                            ))}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+}
