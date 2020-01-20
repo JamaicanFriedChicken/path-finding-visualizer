e6b707cc82f18ad089c58902e0fae221f2eced7d
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2020-01-20 23:50:11 +0800

    used heaps to optimize astar algorithm

--- src/algorithms/astar.js
+++ src/algorithms/astar.js
@@ -1,60 +1,87 @@
+import { getUnvisitedNeighbors } from "../algorithms/dijkstra";
+import Heap from "heap";
+var yetToVisit;
+
 export function astar(grid, startNode, finishNode) {
-    const visitedNodesInOrder = [];
-    startNode.distance = 0;
-    const unvisitedNodes = getAllNodes(grid);
-
-    while (!!unvisitedNodes.length) {
-        sortNodesByDistance(unvisitedNodes);
-        const closestNode = unvisitedNodes.shift();
-        if (closestNode.isWall) continue;
-
-        if (closestNode.distance === Infinity) return visitedNodesInOrder;
-        closestNode.isVisited = true;
-        //console.log(closestNode.row + "  " + closestNode.col+"\n");
-
-        visitedNodesInOrder.push(closestNode);
-        if (closestNode === finishNode) return visitedNodesInOrder;
-        updateUnvisitedNeighbors(closestNode, grid, finishNode);
+    if (!startNode || !finishNode || startNode === finishNode) {
+        return false;
     }
-}
-function getAllNodes(grid) {
-    const nodes = [];
-    for (const row of grid) {
-        for (const node of row) {
-            nodes.push(node);
+
+    const visitedNodesInOrder = [];
+
+    yetToVisit = new Heap(function(a, b) {
+        return a.fCost - b.fCost;
+    });
+
+    initializeCosts(grid);
+
+    startNode.fCost = 0;
+    startNode.gCost = 0;
+    startNode.hCost = 0;
+
+    yetToVisit.heapify();
+
+    while (yetToVisit.length !== 0) {
+        const currNode = yetToVisit.pop();
+
+        if (typeof currNode === "undefined") return visitedNodesInOrder;
+
+        if (currNode.isWall) continue;
+        if (currNode.fCost === Infinity) return visitedNodesInOrder;
+
+        currNode.visited = true;
+        console.log(currNode);
+
+        visitedNodesInOrder.push(currNode);
+
+        if (currNode === finishNode) return visitedNodesInOrder;
+
+        const unvisitedNeighbours = getUnvisitedNeighbors(currNode, grid);
+
+        for (const neighbor of unvisitedNeighbours) {
+            if (!neighbor.isWall) {
+                let hCost = manhattanDistance(
+                    neighbor.col,
+                    finishNode.col,
+                    neighbor.row,
+                    finishNode.row
+                );
+
+                if (currNode.gCost + 1 < neighbor.gCost) {
+                    neighbor.hCost = hCost;
+                    neighbor.gCost = currNode.gCost + 1;
+                    neighbor.fCost = neighbor.hCost + neighbor.gCost;
+                    neighbor.previousNode = currNode;
+                    yetToVisit.updateItem(neighbor);
+                }
+            }
         }
     }
-    return nodes;
-}
-function sortNodesByDistance(unvisitedNodes) {
-    unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+    console.log(grid);
+    return visitedNodesInOrder;
 }
 
-function getUnvisitedNeighbors(node, grid) {
-    const neighbors = [];
-    const { col, row } = node;
-    if (row > 0) neighbors.push(grid[row - 1][col]);
-    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
-    if (col > 0) neighbors.push(grid[row][col - 1]);
-    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
-    return neighbors.filter(neighbor => !neighbor.isVisited);
+export function euclideanDistance(colA, colB, rowA, rowB) {
+    const a = Math.abs(colA - colB);
+    const b = Math.abs(rowA - rowB);
+    const aSquared = Math.pow(a, 2);
+    const bSquared = Math.pow(b, 2);
+    return Math.pow(aSquared + bSquared, 0.5);
 }
 
-function manhattanDistance(nodeOne, nodeTwo) {
-    let xChange = Math.abs(nodeOne.row - nodeTwo.row);
-    let yChange = Math.abs(nodeOne.col - nodeTwo.col);
-
-    return xChange + yChange;
+export function manhattanDistance(colA, colB, rowA, rowB) {
+    const a = Math.abs(colA - colB);
+    const b = Math.abs(rowA - rowB);
+    return a + b;
 }
 
-function updateUnvisitedNeighbors(node, grid, finishNode) {
-    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
-    for (const neighbor of unvisitedNeighbors) {
-        neighbor.distance =
-            node.distance +
-            manhattanDistance(neighbor, finishNode) -
-            manhattanDistance(node, finishNode) +
-            1;
-        neighbor.previousNode = node;
+function initializeCosts(grid) {
+    for (let row of grid) {
+        for (let node of row) {
+            node.fCost = Infinity;
+            node.gCost = Infinity;
+            node.hCost = Infinity;
+            yetToVisit.push(node);
+        }
     }
 }
